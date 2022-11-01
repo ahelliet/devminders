ab3c771c09ca13d5bbba9a44c9b3f6f528661f60
Anthony Helliet <8291454-ahelliet@users.noreply.gitlab.com>
2022-11-01 23:59:07 +0100

    tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        minders: "#0B96C2",
+      },
+    },
+  },
+  plugins: [],
+};
